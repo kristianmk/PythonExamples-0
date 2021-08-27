b4ea9884bb2322a6af74cf96fcbd57723cc1c5c0
kristianmk <1713062+kristianmk@users.noreply.github.com>
2021-08-27 13:59:49 +0200

    Create meme.py
    First version of simple MEME retriever.

---
+++ meme.py
@@ -0,0 +1,12 @@
+# pillow is based on PIL, so import Image from PIL
+from PIL import Image
+
+# requests handles the http(s) protocol for you, to simplify getting an image f$
+import requests
+
+
+# URL to a specific MEME on Reddit:
+url = "https://i.redd.it/4fmgvj6odij71.jpg"
+
+img = Image.open(requests.get(url, stream=True).raw)
+img.show()

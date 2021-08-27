17ae327b02a59359645a076017a9232d557fabce
kristianmk <1713062+kristianmk@users.noreply.github.com>
2021-08-27 14:53:06 +0200

    Update upside_down_meme.py
    Removed unused requests import from upside_down_meme.py.

--- upside_down_meme.py
+++ upside_down_meme.py
@@ -1,10 +1,6 @@
 # pillow is based on PIL, so import Image from PIL
 from PIL import Image
 
-# requests handles the http(s) protocol for you, to simplify getting an image
-# from the internet.
-import requests
-
 
 # Upside down meme file (make sure to run from a directory containing this file!):
 fileName = "upside-down-meme.png"

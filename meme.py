5b4443bf6d44534970b39e219b9c20094428d30d
kristianmk <1713062+kristianmk@users.noreply.github.com>
2021-08-27 14:20:12 +0200

    Update meme.py
    Added linebreak to keep on one slide in presentation..

--- meme.py
+++ meme.py
@@ -1,13 +1,13 @@
 # pillow is based on PIL, so import Image from PIL
 from PIL import Image
 
-# requests handles the http(s) protocol for you, to simplify getting an image from the internet.
+# requests handles the http(s) protocol for you, to simplify getting an image
+# from the internet.
 import requests
 
 
 # URL to a specific MEME on Reddit:
 url = "https://i.redd.it/4fmgvj6odij71.jpg"
 
-
-with Image.open(requests.get(url, stream=True).raw) as img:
-  img.show()
+img = Image.open(requests.get(url, stream=True).raw)
+img.show()
